1f8b97467d9c54452a88857b62db78e9953221fb
Katherine Donnally <kbd3ax@virginia.edu>
2019-02-03 16:40:26 -0500

    Create & import CSS file for homepage

--- assets/python/html_to_css.py
+++ assets/python/html_to_css.py
@@ -33,7 +33,7 @@ def classesToCSS(class_names):
 pattern = r'class=".*?"'
 
 # list of all 'class="<val>"' in file
-matches = regexTextSearch('../../pages/projects.html', pattern)
+matches = regexTextSearch('../../pages/home-1.html', pattern)
 
 # list of all val for 'class="<val>"' in matches
 class_names = stripClassStrings(matches)

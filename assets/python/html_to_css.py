e39e3b1a56125f15ee3b7fb33f1004d1701631c9
Katherine Donnally <kbd3ax@virginia.edu>
2019-05-14 18:18:22 -0400

    Create file for new navigation & import it; set up basics on fxns
    - Can change back and forth from vertical to horizontal just with
      CSS which is pretty cool!!

--- assets/python/html_to_css.py
+++ assets/python/html_to_css.py
@@ -33,7 +33,7 @@ def classesToCSS(class_names):
 pattern = r'class=".*?"'
 
 # list of all 'class="<val>"' in file
-matches = regexTextSearch('../../pages/real/new-about.html', pattern)
+matches = regexTextSearch('../../_includes/new-nav.html', pattern)
 
 # list of all val for 'class="<val>"' in matches
 class_names = stripClassStrings(matches)

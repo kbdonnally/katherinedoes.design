d4e38fcdee328121c3401318c8baf139a369c79c
Katherine Donnally <kbd3ax@virginia.edu>
2019-05-25 15:56:01 -0400

    Create & import SCSS file for Hire Me page

--- assets/python/html_to_css.py
+++ assets/python/html_to_css.py
@@ -33,7 +33,7 @@ def classesToCSS(class_names):
 pattern = r'class=".*?"'
 
 # list of all 'class="<val>"' in file
-matches = regexTextSearch('../../_includes/new-nav.html', pattern)
+matches = regexTextSearch('../../pages/real/hire-me.html', pattern)
 
 # list of all val for 'class="<val>"' in matches
 class_names = stripClassStrings(matches)
